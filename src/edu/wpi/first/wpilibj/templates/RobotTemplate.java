9aa1baab897e1867788e7ba0e7f858893963d1e1
Win_XP <1559deviltech@gmail.com>
2013-02-02 11:14:24 -0600

    Changes Jaguar mode for autonomous
    BEWP

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -2,6 +2,7 @@ package edu.wpi.first.wpilibj.templates;
 
 import com.sun.squawk.platform.windows.natives.Time;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 
 
@@ -9,8 +10,8 @@ import edu.wpi.first.wpilibj.*;
 public class RobotTemplate extends SimpleRobot
 {
     DriveThread dthread;
-    Victor leftMotor;
-    Victor rightMotor;
+    CANJaguar leftMotor;
+    CANJaguar rightMotor;
     RobotDrive rd;
     Joystick stick;
     Joystick wheel;
@@ -23,23 +24,89 @@ public class RobotTemplate extends SimpleRobot
     DigitalInput autonomousA;
     DigitalInput autonomousB;
     int time;
+    
 
     
     public void robotInit()
     {
-        leftMotor = new Victor(Wiring.LEFT_WHEEL);
-        rightMotor = new Victor(Wiring.RIGHT_WHEEL);
-        wheel = new Joystick(Wiring.WHEEL);
-        stick = new Joystick(Wiring.THROTTLE);
-        rd = new RobotDrive(leftMotor, rightMotor);
-        dthread = new DriveThread(this, rd, wheel, stick);
-        shootOn = new JoystickButton(stick, Wiring.SHOOTER_ON);
-        shootOff = new JoystickButton(stick, Wiring.SHOOTER_OFF);
-        shooter = new Shooter(Wiring.SHOOTER_MOTOR);
-        hopper = new Hopper(Wiring.HOPPER_SERVO);
-        autonomousA = new DigitalInput(Wiring.AUTONOMOUS_SWITCH_A);
-        autonomousB = new DigitalInput(Wiring.AUTONOMOUS_SWITCH_B);
+        try 
+        {
+            leftMotor = new CANJaguar(Wiring.LEFT_WHEEL);
+            rightMotor = new CANJaguar(Wiring.RIGHT_WHEEL);
+            wheel = new Joystick(Wiring.WHEEL);
+            stick = new Joystick(Wiring.THROTTLE);
+            rd = new RobotDrive(leftMotor, rightMotor);
+            dthread = new DriveThread(this, rd, wheel, stick);
+            shootOn = new JoystickButton(stick, Wiring.SHOOTER_ON);
+            shootOff = new JoystickButton(stick, Wiring.SHOOTER_OFF);
+            shooter = new Shooter(Wiring.SHOOTER_MOTOR);
+            hopper = new Hopper(Wiring.HOPPER_SERVO);
+            autonomousA = new DigitalInput(Wiring.AUTONOMOUS_SWITCH_A);
+            autonomousB = new DigitalInput(Wiring.AUTONOMOUS_SWITCH_B);
+        } 
+        catch (CANTimeoutException ex) 
+        {
+            ex.printStackTrace();
+        }
     }
+    
+     // Configure a Jaguar for Position mode
+    public void cfgPosMode(CANJaguar jag)
+    {
+        try
+        {
+            jag.disableControl();
+            jag.changeControlMode(CANJaguar.ControlMode.kPosition);
+            jag.setPositionReference(CANJaguar.PositionReference.kQuadEncoder);
+            jag.setPID(0.0, 0.0, 0.0);
+            jag.configEncoderCodesPerRev((Wiring.TICKSPERREV * Wiring.WHEELSPROCKET) / Wiring.DRIVESPROCKET);
+            jag.configMaxOutputVoltage(Wiring.MAXJAGVOLTAGE);
+            //jag.setVoltageRampRate(50);
+            jag.enableControl();
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+    }   //  cfgPosMode
+
+    // Configure a Jaguar for Speed mode
+    public void cfgSpeedMode(CANJaguar jag)
+    {
+        try
+        {
+            jag.disableControl();
+            jag.changeControlMode(CANJaguar.ControlMode.kSpeed);
+            jag.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
+            jag.setPID(Wiring.P,Wiring.I,Wiring.D);
+	    //jag.setPID(0.76, 0.046, 0.0);
+            jag.configEncoderCodesPerRev((Wiring.TICKSPERREV * Wiring.WHEELSPROCKET) / Wiring.DRIVESPROCKET);
+	    //jag.configMaxOutputVoltage(MAXJAGVOLTAGE);
+            //jag.setVoltageRampRate(50);
+            jag.enableControl();
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+    }   //  cfgSpeedMode
+
+    // Configure a Jaguar for normal (PWM like) mode
+    public void cfgNormalMode(CANJaguar jag)
+    {
+        try
+        {
+            jag.disableControl();
+            jag.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+            jag.configMaxOutputVoltage(Wiring.MAXJAGVOLTAGE);
+            jag.enableControl();      
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+    }   //  cfgNormalMode
+
     
     public void autonomous()
     {
@@ -56,7 +123,10 @@ public class RobotTemplate extends SimpleRobot
 	{
 		time = 11;
 	}
-
+        
+        cfgPosMode(leftMotor);
+        cfgPosMode(rightMotor);
+        
         //shoot 3 with 1s delay in between
 	while(isAutonomous())
 	{
@@ -68,8 +138,12 @@ public class RobotTemplate extends SimpleRobot
                 Timer.delay(1);
                 hopper.load();
 	}
+        
+        cfgNormalMode(leftMotor);
+        cfgNormalMode(rightMotor);
+        
 }
-
+        
 
     public void operatorControl()
     {
--- src/edu/wpi/first/wpilibj/templates/Wiring.java
+++ src/edu/wpi/first/wpilibj/templates/Wiring.java
@@ -3,32 +3,41 @@ package edu.wpi.first.wpilibj.templates;
 public class Wiring
 {
     //wheel buttons
-    static public final int X_BUTTON = 1; //x
-    static public final int SQUARE_BUTTON = 2;//square
-    static public final int CIRCLE_BUTTON = 3;//circle
-    static public final int TRIANGLE_BUTTON = 4;//triangle
-    static public final int RIGHT_TICK = 5;//r1
-    static public final int LEFT_TICK = 6;//l1
-    static public final int R2_BUTTON = 7;//r2
-    static public final int L2_BUTTON = 8;//l2
-    static public final int SELECT_BUTTON = 9;//select
-    static public final int START_BUTTON = 10;//start
-    static public final int SHOOTER_OFF = 11;//r3
-    static public final int SHOOTER_ON = 12;//l3
+    static public final int X_BUTTON            = 1; //x
+    static public final int SQUARE_BUTTON       = 2;//square
+    static public final int CIRCLE_BUTTON       = 3;//circle
+    static public final int TRIANGLE_BUTTON     = 4;//triangle
+    static public final int RIGHT_TICK          = 5;//r1
+    static public final int LEFT_TICK           = 6;//l1
+    static public final int R2_BUTTON           = 7;//r2
+    static public final int L2_BUTTON           = 8;//l2
+    static public final int SELECT_BUTTON       = 9;//select
+    static public final int START_BUTTON        = 10;//start
+    static public final int SHOOTER_OFF         = 11;//r3
+    static public final int SHOOTER_ON          = 12;//l3
     //Joystick
-    static public final int TRIGGER = 1;
+    static public final int TRIGGER             = 1;
     //Motors
-    static public final int LEFT_WHEEL = 2;
-    static public final int RIGHT_WHEEL = 4;
-    static public final int SHOOTER_MOTOR = 8;
-    static public final int HOPPER_SERVO = 7;
+    static public final int LEFT_WHEEL          = 10;
+    static public final int RIGHT_WHEEL         = 7;
+    static public final int SHOOTER_MOTOR       = 8;
+    static public final int HOPPER_SERVO        = 7;
     //Digital I/O
     static public final int AUTONOMOUS_SWITCH_A = 4;
     static public final int AUTONOMOUS_SWITCH_B = 5;
     //Joysticks
-    static public final int WHEEL = 2;
-    static public final int THROTTLE = 1;
-    //timer delay for turning
-    static public final double TURN_DELAY = 1.0/16.0;
+    static public final int WHEEL               = 2;
+    static public final int THROTTLE            = 1;
+    //MISC   
+    static public final double TURN_DELAY       = 1.0/16.0;
+    static public final int ENCODER             = 1;
+    static public final double P                = .76;
+    static public final double I                = .46;
+    static public final double D                = 0;
+    static public final int TICKSPERREV         = 250;
+    static public final int WHEELSPROCKET       = 42;
+    static public final int DRIVESPROCKET       = 38;
+    static public final int MAXJAGVOLTAGE       = 12;
+    
 }
 
